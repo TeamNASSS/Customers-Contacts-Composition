11337e649718e9d234696518f6e8acecb04da374
Sowrya Gali <galisowrya@gmail.com>
2022-12-16 17:37:27 -0500

    Added files for CI/CD

---
+++ aws/scripts/application-stop.sh
@@ -0,0 +1,2 @@
+cd /home/ec2-user/Customers-Contacts-Composition
+pkill -9 -f "python3 main.py"
---
+++ aws/scripts/install-dependencies.sh
@@ -0,0 +1,5 @@
+sudo pip3 install virtualenv
+cd /home/ec2-user/Customers-Contacts-Composition
+virtualenv environment
+source environment/bin/activate
+sudo pip3 install -r requirements.txt
